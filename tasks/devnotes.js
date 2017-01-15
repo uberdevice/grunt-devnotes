2197c5410c0731d66c753ac80b5077e74b2395f9
= <=>
2017-01-15 14:01:07 -0600

    Breaking changes.

--- tasks/devnotes.js
+++ tasks/devnotes.js
@@ -6,3 +6,16 @@ var exec = require('child_process').exec;
 exports.printMsg = function() {
     console.log("This is a message from the devnotes package");
 };
+
+exports.updateDevNotes = function() {
+    exec('echo blah', function(err, stdout) => {
+        if (err) {
+            grunt.fatal('Can not get module information. ${err}')
+            return err;
+        } else {
+            console.log(stdout + err);
+            return stdout;
+        };
+
+    });
+};
